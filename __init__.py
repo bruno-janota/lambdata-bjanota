496df9ea058359dee2b171a9713fa781d2da3528
bruno-janota <bruno-janota@users.noreply.github.com>
2019-09-04 00:20:33 -0400

    Added sklearn to Pipfile and train_valid_test_split function

--- __init__.py
+++ __init__.py
@@ -4,12 +4,12 @@ lambdata-bjanota - A Collection of Data Science helper functions.
 
 import pandas as pd
 import numpy as np
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
 ZEROS = np.zeros(5)
 ONES = np.ones(10)
 
-""" def train_validation_test_split(
+def train_validation_test_split(
     X, y, train_size=0.7, val_size=0.1, test_size=0.2, 
     random_state=None, shuffle=True):
         
@@ -22,4 +22,4 @@ ONES = np.ones(10)
         X_train_val, y_train_val, test_size=val_size/(train_size+val_size), 
         random_state=random_state, shuffle=shuffle)
     
-    return X_train, X_val, X_test, y_train, y_val, y_test """
+    return X_train, X_val, X_test, y_train, y_val, y_test
